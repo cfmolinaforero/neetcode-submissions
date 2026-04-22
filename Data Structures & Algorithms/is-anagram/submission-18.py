52a3e4339670b507d8781917b979656a73f1d3eb
Carlos Molina <cfmolinaforero@gmail.com>
2026-04-22 00:48:33 -0400

    Add: is-anagram - submission-18

---
+++ Data Structures & Algorithms/is-anagram/submission-18.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        sChars = {}
+        for c in s:
+            sChars[c] = sChars.get(c, 0) + 1
+        
+        for c in t:
+            if c not in sChars:
+                return False
+            else: 
+                sChars[c] -= 1
+                if sChars[c] == 0:
+                    del sChars[c]
+
+        return True
